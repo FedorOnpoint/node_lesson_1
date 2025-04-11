4f7b835717bc8d2dd71c0cb45f3ebaeb62fa1448
Fedor Barykin <f.barykin@onpoint.ru>
2025-04-11 11:14:07 +0300

    finish

--- bin/index.js
+++ bin/index.js
@@ -3,13 +3,35 @@
 import yargs from "yargs";
 import { hideBin } from "yargs/helpers";
 
-const argv = yargs(hideBin(process.argv)).parse();
-// console.log(argv);
-// console.log(data);
-
-const dateNow = new Date();
 const log = console.log;
+const argv = yargs(hideBin(process.argv)).parse();
+const dateNow = new Date();
 const month = argv.month || argv.m ? dateNow.getMonth() : false;
 const year = argv.year || argv.y ? dateNow.getFullYear() : false;
 const day = argv.day || argv.d ? dateNow.getUTCDate() : false;
-log(month, year, day);
+const action = (action) => {
+  return argv._.includes(action);
+};
+
+if (action("current")) {
+  month && log(month);
+  year && log(year);
+  day && log(day);
+}
+if (action("add")) {
+  month && dateNow.setMonth(month + (argv.m || argv.month));
+  year && dateNow.setFullYear(year + (argv.y || argv.year));
+  day && dateNow.setUTCDate(day + (argv.d || argv.day));
+  log(dateNow.toDateString());
+}
+
+if (action("sub")) {
+  month && dateNow.setMonth(month - (argv.m || argv.month));
+  year && dateNow.setFullYear(year - (argv.y || argv.year));
+  day && dateNow.setUTCDate(day - (argv.d || argv.day));
+  log(dateNow.toDateString());
+}
+
+if (action("date")) {
+  log(dateNow.toDateString());
+}
